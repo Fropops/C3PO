701b00adc54b03c4e0203adab8ee6f9b8feb5a45
Olivier POTTIEZ <olivier.pottiez@live.fr>
2022-05-04 16:39:56 +0200

    Adding mkdir & rmdir Cleanup

---
+++ Agent/Commands/FileSystem/DeleteDirectoryCommand.cs
@@ -0,0 +1,35 @@
+﻿using Agent.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Commands
+{
+    public class MakeDirectoryCommand : AgentCommand
+    {
+        public override string Name => "mkdir";
+        public override void InnerExecute(AgentTask task, Models.Agent agent, AgentTaskResult result, CommModule commm)
+        {
+            string path;
+            if (task.SplittedArgs.Length != 0)
+            {
+                result.Result = $"Usage : {this.Name} folder_to_create";
+                return;
+            }
+                
+            path = task.SplittedArgs[0];
+
+            Directory.Delete(path);
+            if (!Directory.Exists(path))
+            {
+                result.Result = $"{path} deleted";
+                return;
+            }
+
+            result.Result = $"Failed to delete {path}";
+        }
+    }
+}
---
+++ Agent/Commands/FileSystem/MakeDirectoryCommand.cs
@@ -0,0 +1,29 @@
+﻿using Agent.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Commands
+{
+    public class DeleteDirectoryCommand : AgentCommand
+    {
+        public override string Name => "rmdir";
+        public override void InnerExecute(AgentTask task, Models.Agent agent, AgentTaskResult result, CommModule commm)
+        {
+            string path;
+            if (task.SplittedArgs.Length != 0)
+            {
+                result.Result = $"Usage : {this.Name} folder_to_delete";
+                return;
+            }
+                
+            path = task.SplittedArgs[0];
+
+            var dirInfo = Directory.CreateDirectory(path);
+            result.Result = $"{dirInfo.FullName} created";
+        }
+    }
+}

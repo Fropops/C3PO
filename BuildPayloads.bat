f0c15feb9cdf2b4e43ecb5af7e8526020a29a42d
Olivier POTTIEZ <olivier.pottiez@live.fr>
2023-05-19 17:32:30 +0200

    A New Hope

--- BuildPayloads.bat
+++ BuildPayloads.bat
@@ -1,17 +1,17 @@
 set msbuild="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-set buildDir=e:\Share\Projects\C2Sharp\tmpbuild
+set buildDir=e:\Share\Projects\C3PO\tmpbuild
 
-set agentProj=e:\Share\Projects\C2Sharp\Agent\Agent.csproj
-set decoderProj=E:\Share\Projects\C2Sharp\Payload\DecoderDll\DecoderDll.csproj
-set patcherProj=E:\Share\Projects\C2Sharp\Payload\PatcherDll\PatcherDll.csproj
-set injectProj=E:\Share\Projects\C2Sharp\Payload\InjectDll\InjectDll.csproj
-set starterProj=E:\Share\Projects\C2Sharp\Payload\Starter\Starter.csproj
-set serviceProj=E:\Share\Projects\C2Sharp\Payload\Service\Service.csproj
+set agentProj=e:\Share\Projects\C3PO\Agent\Agent.csproj
+set decoderProj=E:\Share\Projects\C3PO\Payload\DecoderDll\DecoderDll.csproj
+set patcherProj=E:\Share\Projects\C3PO\Payload\PatcherDll\PatcherDll.csproj
+set injectProj=E:\Share\Projects\C3PO\Payload\InjectDll\InjectDll.csproj
+set starterProj=E:\Share\Projects\C3PO\Payload\Starter\Starter.csproj
+set serviceProj=E:\Share\Projects\C3PO\Payload\Service\Service.csproj
 
-set destx86Dir=E:\Share\Projects\C2Sharp\Payloads\x86
-set destx64Dir=E:\Share\Projects\C2Sharp\Payloads\x64
-set destdebugDir=E:\Share\Projects\C2Sharp\Payloads\debug
-set scriptDir=E:\Share\Projects\C2Sharp\Payload\Scripts
+set destx86Dir=E:\Share\Projects\C3PO\Payloads\x86
+set destx64Dir=E:\Share\Projects\C3PO\Payloads\x64
+set destdebugDir=E:\Share\Projects\C3PO\Payloads\debug
+set scriptDir=E:\Share\Projects\C3PO\Payload\Scripts
 
 del %destx86Dir%\* /f /q
 del %destx64Dir%\* /f /q

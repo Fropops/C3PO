48d5d3972680f212afa1a0e95259fbc12312f78d
Olivier POTTIEZ <olivier.pottiez@live.fr>
2023-05-20 14:35:11 +0200

    Fix Payload Script & Service

--- BuildPayloadElements.bat
+++ BuildPayloadElements.bat
@@ -1,3 +1,5 @@
+cls
+
 set msbuild="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
 set buildDir=e:\Share\Projects\C3PO\tmpbuild
 
@@ -23,11 +25,11 @@ copy %scriptDir%\*.* %destx64Dir%\
 copy %scriptDir%\*.* %destdebugDir%\ 
 
 echo Agent
-%msbuild% %agentproj% /p:configuration=releasex86 /p:platform=x86 /p:outputpath=%builddir%
+%msbuild% %agentproj% /p:configuration=release /p:platform=x86 /p:outputpath=%builddir%
 copy %builddir%\Agent.exe %destx86Dir%\
 del %builddir%\* /f /q
 
-%msbuild% %agentproj% /p:configuration=releasex64 /p:platform=x64 /p:outputpath=%builddir%
+%msbuild% %agentproj% /p:configuration=release /p:platform=x64 /p:outputpath=%builddir%
 copy %builddir%\Agent.exe %destx64Dir%\
 del %builddir%\* /f /q
 
@@ -36,11 +38,11 @@ copy %builddir%\Agent.exe %destdebugDir%\
 del %builddir%\* /f /q
 
 echo Patcher
-%msbuild% %patcherProj% /p:configuration=releasex86 /p:platform=x86 /p:outputpath=%builddir%
+%msbuild% %patcherProj% /p:configuration=release /p:platform=x86 /p:outputpath=%builddir%
 copy %builddir%\Patcher.dll %destx86Dir%\
 del %builddir%\* /f /q
 
-%msbuild% %patcherProj% /p:configuration=releasex64 /p:platform=x64 /p:outputpath=%builddir%
+%msbuild% %patcherProj% /p:configuration=release /p:platform=x64 /p:outputpath=%builddir%
 copy %builddir%\Patcher.dll %destx64Dir%\
 del %builddir%\* /f /q
 
@@ -49,11 +51,11 @@ copy %builddir%\Patcher.dll %destdebugDir%\
 del %builddir%\* /f /q
 
 echo Inject
-%msbuild% %injectProj% /p:configuration=releasex86 /p:platform=x86 /p:outputpath=%builddir%
+%msbuild% %injectProj% /p:configuration=release /p:platform=x86 /p:outputpath=%builddir%
 copy %builddir%\Inject.dll %destx86Dir%\
 del %builddir%\* /f /q
 
-%msbuild% %injectProj% /p:configuration=releasex64 /p:platform=x64 /p:outputpath=%builddir%
+%msbuild% %injectProj% /p:configuration=release /p:platform=x64 /p:outputpath=%builddir%
 copy %builddir%\Inject.dll %destx64Dir%\
 del %builddir%\* /f /q
 
@@ -63,11 +65,11 @@ del %builddir%\* /f /q
 
 
 echo Starter
-%msbuild% %starterProj% /p:configuration=releasex86 /p:platform=x86 /p:outputpath=%builddir%
+%msbuild% %starterProj% /p:configuration=release /p:platform=x86 /p:outputpath=%builddir%
 copy %builddir%\Starter.exe %destx86Dir%\
 del %builddir%\* /f /q
 
-%msbuild% %starterProj% /p:configuration=releasex64 /p:platform=x64 /p:outputpath=%builddir%
+%msbuild% %starterProj% /p:configuration=release /p:platform=x64 /p:outputpath=%builddir%
 copy %builddir%\Starter.exe %destx64Dir%\
 del %builddir%\* /f /q
 
@@ -76,11 +78,11 @@ copy %builddir%\Starter.exe %destdebugDir%\
 del %builddir%\* /f /q
 
 echo Service
-%msbuild% %serviceProj% /p:configuration=releasex86 /p:platform=x86 /p:outputpath=%builddir%
+%msbuild% %serviceProj% /p:configuration=release /p:platform=x86 /p:outputpath=%builddir%
 copy %builddir%\Service.exe %destx86Dir%\
 del %builddir%\* /f /q
 
-%msbuild% %serviceProj% /p:configuration=releasex64 /p:platform=x64 /p:outputpath=%builddir%
+%msbuild% %serviceProj% /p:configuration=release /p:platform=x64 /p:outputpath=%builddir%
 copy %builddir%\Service.exe %destx64Dir%\
 del %builddir%\* /f /q
 
